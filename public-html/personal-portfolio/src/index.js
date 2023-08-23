8a7425f8e2b694a70e31b993fe883c6d4879a97c
code-cody-418 <code_slayer1@protonmail.com>
2023-08-23 07:15:36 -0600

    set up

--- public-html/personal-portfolio/src/index.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import {App} from "./App";
-import "bootstrap/dist/css/bootstrap.css"
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(<App tab="home" />);
